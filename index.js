ac3b46d2b3255408cf2e04cb22569cb2440366fc
paulapjpiolin <paula.pjpiolin@gmail.com>
2026-03-07 15:25:29 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ greeting.style.textAlign = "center";
 greeting.style.marginTop = "50px";
 const username = window.prompt("What is your name?", "your name here");
 greeting.innerHTML = "Welcome to my website," + username;
+

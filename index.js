247723a3ba63993747a1333a5d817e1fcbcadb08
paulapjpiolin <paula.pjpiolin@gmail.com>
2026-03-13 11:29:14 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const greeting = document.getElementById("greeting");
-greeting.style.textAlign = "center";
-greeting.style.marginTop = "50px";
-const username = window.prompt("What is your name?", "your name here");
-greeting.innerHTML = "Welcome to my website," + username;
-
